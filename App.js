4a42eaa32c8ce84fddcab5233d2dcbb3654829d5
Rebekah <rebekahjanedejong@gmail.com>
2021-12-24 15:37:07 -0500

    Update title

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: 'Home',
     defaultNavigationOptions: {
-      title: 'Hogwarts'
+      title: 'Hogwarts Directory'
     }
   }
 )
